f7d0b6a6636dc79e06b9d833741224d08b423fe2
PabloBlancoArg <blancopablo1991@gmail.com>
2022-03-15 14:22:01 -0300

    agregue link de deploy en netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ const Footer = () => {
   return (
     <div className='py-3 mt-5 bg-gray-100 max-w mx-auto'>
         <h1 className='font-bold max-w-5xl mx-auto text-green-800 text-center'><a href='https://github.com/PabloBlancoArg/saludable2-vite'>https://github.com/PabloBlancoArg/saludable2-vite</a></h1>
+        <h1 className='font-bold max-w-5xl mx-auto text-green-800 text-center'><a href='https://reverent-bhaskara-9ba73f.netlify.app/'>https://reverent-bhaskara-9ba73f.netlify.app/</a></h1>
+        
     </div>
   )
 }
