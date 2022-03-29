2485263870eb0a8c92fcb5a2183a95b65cb1b810
PabloBlancoArg <blancopablo1991@gmail.com>
2022-03-29 12:12:26 -0300

    add link

--- src/paginas/LoginForm.jsx
+++ src/paginas/LoginForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
   return (
     
     <div>
-        LoginForm
+       <h1>Login Form</h1>
+        <Link to="/o">Tiendas</Link>
     </div>
   )
 }
